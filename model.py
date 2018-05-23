98efd8ac4fd3678e41148f6cfedf70d630ffe5d3
jixj <jixiaojie@tom.com>
2018-05-24 00:52:19 +0800

    upload model

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-#import os
 import csv
 
 import cv2
@@ -19,14 +18,16 @@ samples = []
 with open('./driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
-        line[0] = line[0].replace('\\','/')
-        line[1] = line[1].replace('\\','/')
-        line[2] = line[2].replace('\\','/')
-        samples.append(line)
+        #if steering value large than 0.6 , do not use to train set 
+        if abs(float(line[3])) <= 0.98:
+            line[0] = line[0].replace('\\','/')
+            line[1] = line[1].replace('\\','/')
+            line[2] = line[2].replace('\\','/')
+            samples.append(line)
        
 
-
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
 
 # Data generator 
 # Because the generator is slower than load all data in the momery, so not to use here
@@ -49,11 +50,11 @@ def generator(samples, batch_size=32):
             # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
-            return sklearn.utils.shuffle(X_train, y_train)
+            yield (X_train, y_train)
+
 
 # Getdata by driving_log.csv
 def getdata(samples):
-    #shuffle(samples)
     sampleslen = len(samples) * 3
     correction = 0.2
     images = []
@@ -92,18 +93,15 @@ def getdata(samples):
         right_angle = float(batch_sample[3]) - correction
         images.append(right_image)
         angles.append(right_angle)
-#        images.append(np.fliplr(right_image))
-#        angles.append(-right_angle)
         num += 1
 
         if (num % 1000 == 0):
             print('Loaded ', num , '	', num * 1.0 / sampleslen)
 
-        # trim image to only see section with road
+    # trim image to only see section with road
     X_train = np.array(images)
     y_train = np.array(angles)
 
-    #return sklearn.utils.shuffle(X_train, y_train)
     return X_train, y_train
 
 
@@ -115,19 +113,21 @@ print()
 print('Loading train data , total ', len(validation_samples) * 3)
 X_valid, y_valid = getdata(validation_samples)
 
+
 #Trimmed image format
 ch, row, col = 3, 160, 320 
 
 
 #Use NVIDIA's model
 model = Sequential()
-#Crop image
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(row, col, ch)))
 
 # Preprocess incoming data, centered around zero with small standard deviation 
 model.add(Lambda(lambda x: x/127.5 - 1.,
-        input_shape=(row - 70 , col, ch),
-        output_shape=(row - 70, col, ch)))
+        input_shape=(row, col, ch),
+        output_shape=(row, col, ch)))
+
+#Crop image
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(row, col, ch)))
 
 # Add Convolution layers
 model.add(Conv2D(24, (5, 5)))
@@ -154,7 +154,7 @@ model.add(Activation('relu'))
 model.add(Flatten())
 
 # Add fully connection layer
-model.add(Dense(1164, kernel_initializer = TruncatedNormal(mean=0.0, stddev=0.05, seed=None), bias_initializer='zeros'))
+model.add(Dense(1164, kernel_initializer = TruncatedNormal(mean=0.0, stddev=0.1, seed=None), bias_initializer='zeros'))
 model.add(Activation('relu'))
 
 model.add(Dense(100))
